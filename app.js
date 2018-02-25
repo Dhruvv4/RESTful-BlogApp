1244a73f4026d583bdb23a4c1a3aa4570c5079e4
Dhruvv4 <dhruvvaghela17@gmail.com>
2018-02-25 18:08:28 +0530

    Database connected and localhost initialized.

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+// Essential imports
 var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose");
 
+
 var app = express();
 
+mongoose.connect("mongodb://localhost/restful_blog_app");
 
+// Setting the view engine to 'ejs' files
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000, () => {
+    console.log('RESTful BlogApp successfully started...');
+});
